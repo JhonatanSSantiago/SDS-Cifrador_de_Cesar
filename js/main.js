3f71ddc738f1f5f209286aec9ff5fe206f3a881e
Jhonatan Santiago <jhonssantiago@gmail.com>
2022-03-03 10:12:48 -0300

    Personalizando e corrigindo bugs

--- js/main.js
+++ js/main.js
@@ -11,22 +11,27 @@ document.getElementById("btn_cifrar").addEventListener("click",
 function cifrarMsg(){
     var msg = document.getElementById("msg").value.toLowerCase();
     var chave = parseInt(document.getElementById("chave").value);
-    var msg_cifrada = '';  
-    for (var i = 0; i < msg.length; i++ ){
-        if (msg[i] == espaco){
-            msg_cifrada = msg_cifrada + espaco;               
-        }else{
-            var letra_msg = msg[i];
-            var letra_alf = letras_alf.lastIndexOf(letra_msg);
-            if ( (letra_alf + chave) > 25)  {
-                var resto = (parseInt(letra_alf) + chave) - 25;
-                msg_cifrada = msg_cifrada + letras_alf[resto - 1];     
-            } else{            
-                msg_cifrada = msg_cifrada + letras_alf[parseInt(letra_alf) + chave];    
-            }                   
-        }    
-    } 
-    resposta(msg_cifrada);
+    if (chave < 0 || chave > 50){
+        alert("Chave precisa ser entre 0 e 50");
+    } else{
+        var msg_cifrada = '';  
+        for (var i = 0; i < msg.length; i++ ){
+            if (msg[i] == espaco){
+                msg_cifrada = msg_cifrada + espaco;               
+            }else{
+                var letra_msg = msg[i];
+                var letra_alf = letras_alf.lastIndexOf(letra_msg);
+                if ( (letra_alf + chave) > 25)  {
+                    var resto = (parseInt(letra_alf) + chave) - 25;
+                    msg_cifrada = msg_cifrada + letras_alf[resto - 1];     
+                } else{            
+                    msg_cifrada = msg_cifrada + letras_alf[parseInt(letra_alf) + chave];    
+                }                   
+            }    
+        } 
+        resposta(msg_cifrada);
+    }
+   
 }
 
 document.getElementById("btn_descifrar").addEventListener("click",
@@ -45,10 +50,10 @@ function descifrarMsg(){
         if (msg[i] == espaco){
             msg_cifrada = msg_cifrada + espaco; 
         }else{
-            var letra = msg[i];
-            var letra_alf = letras_alf.lastIndexOf(letra); 
+            var letra_msg = msg[i];
+            var letra_alf = letras_alf.lastIndexOf(letra_msg); 
             if ( (letra_alf - chave) < 0)  {
-                var resto = letra_alf - chave;
+                var resto = (parseInt(letra_alf) - chave) + 24;
                 msg_cifrada = msg_cifrada +  letras_alf[2 + resto];    
             }else{
                 msg_cifrada = msg_cifrada +  letras_alf[parseInt(letra_alf) - chave];    
@@ -60,18 +65,22 @@ function descifrarMsg(){
 
 function resposta(msg_cifrada){
     let div = document.createElement("div");
+    div.setAttribute("class", "div_msg");
+    let p = document.createElement("p");
 	let noTxt = document.createTextNode(msg_cifrada.toUpperCase());
-	div.appendChild(noTxt);
+    p.setAttribute("class", "text_msg");
+	p.appendChild(noTxt);
+    div.appendChild(p);
     let botao=document.createElement("input");
     botao.setAttribute("type","button");
-    botao.setAttribute("value","Remover");
+    botao.setAttribute("class","btn btn-danger");
+    botao.setAttribute("value","Apagar");
     botao.onclick=remover;
     div.appendChild(botao);
     document.getElementById("resposta").appendChild(div);
 }
 
-let remover=function()
-{
+let remover=function(){
     let pai=this.parentNode;
     pai.parentNode.removeChild(pai);
 }
